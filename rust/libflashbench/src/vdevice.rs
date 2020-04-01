1828e0884999bf1a80c5e0262cbc769e000730d4
Hyeon Kim <simnalamburt@gmail.com>
2020-04-01 17:38:06 +0900

    libflashbench: Move all constants to the front of files

--- rust/libflashbench/src/vdevice.rs
+++ rust/libflashbench/src/vdevice.rs
@@ -3,6 +3,52 @@
 
 use core::ffi::c_void;
 
+pub const BGC_TH_NR_BLKS: u32 = 14;
+pub const BGC_TH_WB_UTIL: u32 = 5;
+pub const BGC_TH_INTV: u32 = 5000;
+pub const NUM_MAX_ENTRIES_OPR_QUEUE: u32 = 4;
+pub const TBLOCK: u32 = 100;
+pub const TPLOCK: u32 = 100;
+pub const TBERS: u32 = 5000;
+pub const TPROG: u32 = 800;
+pub const TREAD: u32 = 80;
+pub const SECTOR_SIZE: u32 = 512;
+pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
+pub const LOGICAL_PAGE_SIZE: u32 = 4096;
+pub const NUM_LOG_PAGES: u32 = 236390;
+pub const NUM_PAGES_IN_WRITE_BUFFER: u32 = 8;
+pub const NUM_PAGES: u32 = 65664;
+pub const NUM_BLOCKS: u32 = 342;
+pub const NUM_CHIPS: u32 = 2;
+pub const LP_PAGE_SHIFT: u32 = 2;
+pub const LP_PAGE_MASK: u32 = 3;
+pub const NR_LP_IN_PP: u32 = 4;
+// v-layer * h-layer * multi-level degree
+pub const CFACTOR_PERCENT: u32 = 90;
+pub const NUM_PAGES_PER_BLOCK: u32 = 192;
+pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
+pub const NUM_CHIPS_PER_BUS: u32 = 2;
+
+//
+// Hardware configuration
+//
+pub const NUM_BUSES: u32 = 1;
+pub const NR_MAX_LPGS_COPY: u32 = 6144;
+pub const NUM_BTODS: u32 = 2048;
+pub const NUM_WTODS: u32 = 2048;
+
+//
+// Parameterf for DEL manager
+//
+pub const NR_MAX_LPAS_DISCARD: u32 = 2048;
+
+pub type fb_dev_op_t = u32;
+pub const OP_BERS: fb_dev_op_t = 4;
+pub const OP_BLOCK: fb_dev_op_t = 3;
+pub const OP_PLOCK: fb_dev_op_t = 2;
+pub const OP_PROG: fb_dev_op_t = 1;
+pub const OP_READ: fb_dev_op_t = 0;
+
 extern "C" {
     pub type fb_bus_controller_t;
     pub type fb_bio_t;
@@ -60,48 +106,6 @@ pub struct vdevice_block_t {
 pub struct vdevice_page_t {
     pub ptr_data: *mut u8,
 }
-pub const BGC_TH_NR_BLKS: u32 = 14;
-pub const BGC_TH_WB_UTIL: u32 = 5;
-pub const BGC_TH_INTV: u32 = 5000;
-pub const NUM_MAX_ENTRIES_OPR_QUEUE: u32 = 4;
-pub const TBLOCK: u32 = 100;
-pub const TPLOCK: u32 = 100;
-pub const TBERS: u32 = 5000;
-pub const TPROG: u32 = 800;
-pub const TREAD: u32 = 80;
-pub const SECTOR_SIZE: u32 = 512;
-pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
-pub const LOGICAL_PAGE_SIZE: u32 = 4096;
-pub const NUM_LOG_PAGES: u32 = 236390;
-pub const NUM_PAGES_IN_WRITE_BUFFER: u32 = 8;
-pub const NUM_PAGES: u32 = 65664;
-pub const NUM_BLOCKS: u32 = 342;
-pub const NUM_CHIPS: u32 = 2;
-pub const LP_PAGE_SHIFT: u32 = 2;
-pub const LP_PAGE_MASK: u32 = 3;
-pub const NR_LP_IN_PP: u32 = 4;
-// v-layer * h-layer * multi-level degree
-pub const CFACTOR_PERCENT: u32 = 90;
-pub const NUM_PAGES_PER_BLOCK: u32 = 192;
-pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
-pub const NUM_CHIPS_PER_BUS: u32 = 2;
-//
-// Hardware configuration
-//
-pub const NUM_BUSES: u32 = 1;
-pub const NR_MAX_LPGS_COPY: u32 = 6144;
-pub const NUM_BTODS: u32 = 2048;
-pub const NUM_WTODS: u32 = 2048;
-//
-// Parameterf for DEL manager
-//
-pub const NR_MAX_LPAS_DISCARD: u32 = 2048;
-pub type fb_dev_op_t = u32;
-pub const OP_BERS: fb_dev_op_t = 4;
-pub const OP_BLOCK: fb_dev_op_t = 3;
-pub const OP_PLOCK: fb_dev_op_t = 2;
-pub const OP_PROG: fb_dev_op_t = 1;
-pub const OP_READ: fb_dev_op_t = 0;
 #[no_mangle]
 pub unsafe extern "C" fn create_vdevice() -> *mut vdevice_t {
     let mut current_block: u64;
