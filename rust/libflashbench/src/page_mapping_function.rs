4960994c379797900deea9a861d187995f17dc56
Hyeon Kim <simnalamburt@gmail.com>
2020-04-01 17:16:18 +0900

    libflashbench: Use u32 instead of C2RustUnnamed

--- rust/libflashbench/src/page_mapping_function.rs
+++ rust/libflashbench/src/page_mapping_function.rs
@@ -138,43 +138,42 @@ pub const PAGE_STATUS_DEL: fb_pg_status_t = 3;
 pub const PAGE_STATUS_INVALID: fb_pg_status_t = 2;
 pub const PAGE_STATUS_VALID: fb_pg_status_t = 1;
 pub const PAGE_STATUS_FREE: fb_pg_status_t = 0;
-pub type C2RustUnnamed_1 = u32;
-pub const BGC_TH_NR_BLKS: C2RustUnnamed_1 = 14;
-pub const BGC_TH_WB_UTIL: C2RustUnnamed_1 = 5;
-pub const BGC_TH_INTV: C2RustUnnamed_1 = 5000;
-pub const NUM_MAX_ENTRIES_OPR_QUEUE: C2RustUnnamed_1 = 4;
-pub const TBLOCK: C2RustUnnamed_1 = 100;
-pub const TPLOCK: C2RustUnnamed_1 = 100;
-pub const TBERS: C2RustUnnamed_1 = 5000;
-pub const TPROG: C2RustUnnamed_1 = 800;
-pub const TREAD: C2RustUnnamed_1 = 80;
-pub const SECTOR_SIZE: C2RustUnnamed_1 = 512;
-pub const PHYSICAL_PAGE_SIZE: C2RustUnnamed_1 = 16384;
-pub const LOGICAL_PAGE_SIZE: C2RustUnnamed_1 = 4096;
-pub const NUM_LOG_PAGES: C2RustUnnamed_1 = 236390;
-pub const NUM_PAGES_IN_WRITE_BUFFER: C2RustUnnamed_1 = 8;
-pub const NUM_PAGES: C2RustUnnamed_1 = 65664;
-pub const NUM_BLOCKS: C2RustUnnamed_1 = 342;
-pub const NUM_CHIPS: C2RustUnnamed_1 = 2;
-pub const LP_PAGE_SHIFT: C2RustUnnamed_1 = 2;
-pub const LP_PAGE_MASK: C2RustUnnamed_1 = 3;
-pub const NR_LP_IN_PP: C2RustUnnamed_1 = 4;
+pub const BGC_TH_NR_BLKS: u32 = 14;
+pub const BGC_TH_WB_UTIL: u32 = 5;
+pub const BGC_TH_INTV: u32 = 5000;
+pub const NUM_MAX_ENTRIES_OPR_QUEUE: u32 = 4;
+pub const TBLOCK: u32 = 100;
+pub const TPLOCK: u32 = 100;
+pub const TBERS: u32 = 5000;
+pub const TPROG: u32 = 800;
+pub const TREAD: u32 = 80;
+pub const SECTOR_SIZE: u32 = 512;
+pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
+pub const LOGICAL_PAGE_SIZE: u32 = 4096;
+pub const NUM_LOG_PAGES: u32 = 236390;
+pub const NUM_PAGES_IN_WRITE_BUFFER: u32 = 8;
+pub const NUM_PAGES: u32 = 65664;
+pub const NUM_BLOCKS: u32 = 342;
+pub const NUM_CHIPS: u32 = 2;
+pub const LP_PAGE_SHIFT: u32 = 2;
+pub const LP_PAGE_MASK: u32 = 3;
+pub const NR_LP_IN_PP: u32 = 4;
 // v-layer * h-layer * multi-level degree
-pub const CFACTOR_PERCENT: C2RustUnnamed_1 = 90;
-pub const NUM_PAGES_PER_BLOCK: C2RustUnnamed_1 = 192;
-pub const NUM_BLOCKS_PER_CHIP: C2RustUnnamed_1 = 171;
-pub const NUM_CHIPS_PER_BUS: C2RustUnnamed_1 = 2;
+pub const CFACTOR_PERCENT: u32 = 90;
+pub const NUM_PAGES_PER_BLOCK: u32 = 192;
+pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
+pub const NUM_CHIPS_PER_BUS: u32 = 2;
 //
 // Hardware configuration
 //
-pub const NUM_BUSES: C2RustUnnamed_1 = 1;
-pub const NR_MAX_LPGS_COPY: C2RustUnnamed_1 = 6144;
-pub const NUM_BTODS: C2RustUnnamed_1 = 2048;
-pub const NUM_WTODS: C2RustUnnamed_1 = 2048;
+pub const NUM_BUSES: u32 = 1;
+pub const NR_MAX_LPGS_COPY: u32 = 6144;
+pub const NUM_BTODS: u32 = 2048;
+pub const NUM_WTODS: u32 = 2048;
 //
 // Parameterf for DEL manager
 //
-pub const NR_MAX_LPAS_DISCARD: C2RustUnnamed_1 = 2048;
+pub const NR_MAX_LPAS_DISCARD: u32 = 2048;
 #[no_mangle]
 pub unsafe extern "C" fn get_prev_bus_chip(
     mut fb: *mut fb_context_t,
--- rust/libflashbench/src/vdevice.rs
+++ rust/libflashbench/src/vdevice.rs
@@ -60,43 +60,42 @@ pub struct vdevice_block_t {
 pub struct vdevice_page_t {
     pub ptr_data: *mut u8,
 }
-pub type C2RustUnnamed = u32;
-pub const BGC_TH_NR_BLKS: C2RustUnnamed = 14;
-pub const BGC_TH_WB_UTIL: C2RustUnnamed = 5;
-pub const BGC_TH_INTV: C2RustUnnamed = 5000;
-pub const NUM_MAX_ENTRIES_OPR_QUEUE: C2RustUnnamed = 4;
-pub const TBLOCK: C2RustUnnamed = 100;
-pub const TPLOCK: C2RustUnnamed = 100;
-pub const TBERS: C2RustUnnamed = 5000;
-pub const TPROG: C2RustUnnamed = 800;
-pub const TREAD: C2RustUnnamed = 80;
-pub const SECTOR_SIZE: C2RustUnnamed = 512;
-pub const PHYSICAL_PAGE_SIZE: C2RustUnnamed = 16384;
-pub const LOGICAL_PAGE_SIZE: C2RustUnnamed = 4096;
-pub const NUM_LOG_PAGES: C2RustUnnamed = 236390;
-pub const NUM_PAGES_IN_WRITE_BUFFER: C2RustUnnamed = 8;
-pub const NUM_PAGES: C2RustUnnamed = 65664;
-pub const NUM_BLOCKS: C2RustUnnamed = 342;
-pub const NUM_CHIPS: C2RustUnnamed = 2;
-pub const LP_PAGE_SHIFT: C2RustUnnamed = 2;
-pub const LP_PAGE_MASK: C2RustUnnamed = 3;
-pub const NR_LP_IN_PP: C2RustUnnamed = 4;
+pub const BGC_TH_NR_BLKS: u32 = 14;
+pub const BGC_TH_WB_UTIL: u32 = 5;
+pub const BGC_TH_INTV: u32 = 5000;
+pub const NUM_MAX_ENTRIES_OPR_QUEUE: u32 = 4;
+pub const TBLOCK: u32 = 100;
+pub const TPLOCK: u32 = 100;
+pub const TBERS: u32 = 5000;
+pub const TPROG: u32 = 800;
+pub const TREAD: u32 = 80;
+pub const SECTOR_SIZE: u32 = 512;
+pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
+pub const LOGICAL_PAGE_SIZE: u32 = 4096;
+pub const NUM_LOG_PAGES: u32 = 236390;
+pub const NUM_PAGES_IN_WRITE_BUFFER: u32 = 8;
+pub const NUM_PAGES: u32 = 65664;
+pub const NUM_BLOCKS: u32 = 342;
+pub const NUM_CHIPS: u32 = 2;
+pub const LP_PAGE_SHIFT: u32 = 2;
+pub const LP_PAGE_MASK: u32 = 3;
+pub const NR_LP_IN_PP: u32 = 4;
 // v-layer * h-layer * multi-level degree
-pub const CFACTOR_PERCENT: C2RustUnnamed = 90;
-pub const NUM_PAGES_PER_BLOCK: C2RustUnnamed = 192;
-pub const NUM_BLOCKS_PER_CHIP: C2RustUnnamed = 171;
-pub const NUM_CHIPS_PER_BUS: C2RustUnnamed = 2;
+pub const CFACTOR_PERCENT: u32 = 90;
+pub const NUM_PAGES_PER_BLOCK: u32 = 192;
+pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
+pub const NUM_CHIPS_PER_BUS: u32 = 2;
 //
 // Hardware configuration
 //
-pub const NUM_BUSES: C2RustUnnamed = 1;
-pub const NR_MAX_LPGS_COPY: C2RustUnnamed = 6144;
-pub const NUM_BTODS: C2RustUnnamed = 2048;
-pub const NUM_WTODS: C2RustUnnamed = 2048;
+pub const NUM_BUSES: u32 = 1;
+pub const NR_MAX_LPGS_COPY: u32 = 6144;
+pub const NUM_BTODS: u32 = 2048;
+pub const NUM_WTODS: u32 = 2048;
 //
 // Parameterf for DEL manager
 //
-pub const NR_MAX_LPAS_DISCARD: C2RustUnnamed = 2048;
+pub const NR_MAX_LPAS_DISCARD: u32 = 2048;
 pub type fb_dev_op_t = u32;
 pub const OP_BERS: fb_dev_op_t = 4;
 pub const OP_BLOCK: fb_dev_op_t = 3;
