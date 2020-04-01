25e7e1e6e3d9439cc0186a930e8e77660df1e4a5
Hyeon Kim <simnalamburt@gmail.com>
2020-04-01 18:00:00 +0900

    libflashbench: Comment-out unused constants

--- rust/libflashbench/src/gc_page_mapping.rs
+++ rust/libflashbench/src/gc_page_mapping.rs
@@ -1,5 +1,4 @@
-#![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case,
-         non_upper_case_globals)]
+#![allow(mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
 use core::ffi::c_void;
 
@@ -10,37 +9,33 @@ pub const NUM_CHIPS_PER_BUS: u32 = 2;
 // Hardware configuration
 //
 pub const NUM_BUSES: u32 = 1;
-pub const NUM_CHIPS: u32 = 2;
-pub const LOGICAL_PAGE_SIZE: u32 = 4096;
 pub const NR_LP_IN_PP: u32 = 4;
 pub const NUM_PAGES_PER_BLOCK: u32 = 192;
-pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
 
 pub type fb_pg_status_t = u32;
-pub const PAGE_STATUS_VALID: fb_pg_status_t = 1;
-pub const PAGE_STATUS_DEL: fb_pg_status_t = 3;
-pub const PAGE_STATUS_INVALID: fb_pg_status_t = 2;
 pub const PAGE_STATUS_FREE: fb_pg_status_t = 0;
+pub const PAGE_STATUS_VALID: fb_pg_status_t = 1;
+//pub const PAGE_STATUS_INVALID: fb_pg_status_t = 2;
+pub const PAGE_STATUS_DEL: fb_pg_status_t = 3;
 
 pub const BGC_TH_NR_BLKS: u32 = 14;
 pub const BGC_TH_WB_UTIL: u32 = 5;
 pub const BGC_TH_INTV: u32 = 5000;
-pub const NUM_MAX_ENTRIES_OPR_QUEUE: u32 = 4;
-pub const TBLOCK: u32 = 100;
-pub const TPLOCK: u32 = 100;
-pub const TBERS: u32 = 5000;
-pub const TPROG: u32 = 800;
-pub const TREAD: u32 = 80;
+
 pub const SECTOR_SIZE: u32 = 512;
-pub const NUM_LOG_PAGES: u32 = 236390;
+pub const PHYSICAL_PAGE_SIZE: u32 = 16384;
+pub const LOGICAL_PAGE_SIZE: u32 = 4096;
+//pub const NUM_LOG_PAGES: u32 = 236390;
 pub const NUM_PAGES_IN_WRITE_BUFFER: u32 = 8;
 pub const NUM_PAGES: u32 = 65664;
 pub const NUM_BLOCKS: u32 = 342;
-pub const LP_PAGE_SHIFT: u32 = 2;
-pub const LP_PAGE_MASK: u32 = 3;
+pub const NUM_CHIPS: u32 = 2;
+//pub const LP_PAGE_SHIFT: u32 = 2;
+//pub const LP_PAGE_MASK: u32 = 3;
+
 // v-layer * h-layer * multi-level degree
-pub const CFACTOR_PERCENT: u32 = 90;
-pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
+//pub const CFACTOR_PERCENT: u32 = 90;
+//pub const NUM_BLOCKS_PER_CHIP: u32 = 171;
 pub const NR_MAX_LPGS_COPY: u32 = 6144;
 pub const NUM_BTODS: u32 = 2048;
 pub const NUM_WTODS: u32 = 2048;
