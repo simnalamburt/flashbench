c00df7e1b6949e1c22b76ca2c5be8d463c27de4e
Hyeon Kim <simnalamburt@gmail.com>
2020-05-21 19:36:16 +0900

    linux.rs: Clarify names of arguments

--- rust/src/linux.rs
+++ rust/src/linux.rs
@@ -3,13 +3,13 @@ use core::ffi::c_void;
 extern "C" {
     // linux/printk.h
     // TODO: Change into *const u8
-    pub fn printk(fmt: *const i8, _: ...) -> i32;
+    pub fn printk(fmt: *const i8, arguments: ...) -> i32;
 
     // linux/vmalloc.h
     pub fn vmalloc(size: usize) -> *mut c_void;
     pub fn vfree(addr: *const c_void);
 
     // linux/string.h
-    pub fn memset(_: *mut c_void, _: i32, _: usize) -> *mut c_void;
-    pub fn memcpy(_: *mut c_void, _: *const c_void, _: usize) -> *mut c_void;
+    pub fn memset(addr: *mut c_void, byte: i32, size: usize) -> *mut c_void;
+    pub fn memcpy(dest: *mut c_void, src: *const c_void, size: usize) -> *mut c_void;
 }
