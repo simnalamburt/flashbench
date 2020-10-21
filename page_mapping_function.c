59395cc1493b8077a831308db7b9a48ac9f62965
Hyeon Kim <simnalamburt@gmail.com>
2020-10-21 09:22:42 +0900

    page_mapping_function.c: Simplify codes

--- page_mapping_function.c
+++ page_mapping_function.c
@@ -7,32 +7,24 @@
 u32 get_mapped_physical_address(struct fb_context_t *ptr_fb_context,
                                 u32 logical_page_address, u32 *ptr_bus,
                                 u32 *ptr_chip, u32 *ptr_block, u32 *ptr_page) {
-  struct page_mapping_context_t *ptr_mapping_context =
-      (struct page_mapping_context_t *)ptr_fb_context->ptr_mapping_context;
-  u32 physical_page_address = (u32)PAGE_UNMAPPED;
-
-  if (is_valid_address_range(logical_page_address) == false) {
+  if (!is_valid_address_range(logical_page_address)) {
     printk(KERN_ERR
            "flashbench: fb_page_mapping: Invalid logical page range (%d)\n",
            logical_page_address);
-    goto FINISH;
+    return PAGE_UNMAPPED;
   }
 
-  physical_page_address =
-      ptr_mapping_context->ptr_mapping_table->mappings[logical_page_address];
-  if (physical_page_address != (u32)PAGE_UNMAPPED) {
-    convert_to_ssd_layout(physical_page_address, ptr_bus, ptr_chip, ptr_block,
-                          ptr_page);
+  u32 physical_page_address = ptr_fb_context->ptr_mapping_context->ptr_mapping_table->mappings[logical_page_address];
+  if (physical_page_address == (u32)PAGE_UNMAPPED) {
+    return PAGE_UNMAPPED;
   }
 
-FINISH:
+  convert_to_ssd_layout(physical_page_address, ptr_bus, ptr_chip, ptr_block, ptr_page);
   return physical_page_address;
 }
 
 void set_prev_bus_chip(struct fb_context_t *ptr_fb_context, u8 bus, u8 chip) {
-  struct page_mapping_context_t *ptr_mapping_context =
-      (struct page_mapping_context_t *)ptr_fb_context->ptr_mapping_context;
-  struct fb_act_blk_mngr_t *abm = ptr_mapping_context->abm;
+  struct fb_act_blk_mngr_t *abm = ptr_fb_context->ptr_mapping_context->abm;
 
   abm->mru_bus = bus;
   abm->mru_chip = chip;
@@ -40,9 +32,7 @@ void set_prev_bus_chip(struct fb_context_t *ptr_fb_context, u8 bus, u8 chip) {
 
 void get_next_bus_chip(struct fb_context_t *ptr_fb_context, u8 *ptr_bus,
                        u8 *ptr_chip) {
-  struct page_mapping_context_t *ptr_mapping_context =
-      (struct page_mapping_context_t *)ptr_fb_context->ptr_mapping_context;
-  struct fb_act_blk_mngr_t *abm = ptr_mapping_context->abm;
+  struct fb_act_blk_mngr_t *abm = ptr_fb_context->ptr_mapping_context->abm;
 
   if (abm->mru_bus == NUM_BUSES - 1) {
     *ptr_bus = 0;
@@ -75,7 +65,7 @@ void set_curr_gc_block(struct fb_context_t *fb, u32 bus, u32 chip,
 
   *(ftl->gcm->gc_blks + (bus * NUM_CHIPS_PER_BUS + chip)) = blki;
 
-  if (blki != NULL) set_rsv_blk_flag(blki, true);
+  if (blki != NULL) { set_rsv_blk_flag(blki, true); }
 }
 
 struct flash_block *get_curr_active_block(struct fb_context_t *fb, u32 bus,
@@ -91,5 +81,5 @@ void set_curr_active_block(struct fb_context_t *fb, u32 bus, u32 chip,
 
   *(ftl->abm->act_blks + (bus * NUM_CHIPS_PER_BUS + chip)) = blki;
 
-  if (blki != NULL) set_act_blk_flag(blki, true);
+  if (blki != NULL) { set_act_blk_flag(blki, true); }
 }
